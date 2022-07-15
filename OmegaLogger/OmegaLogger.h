4e3804fceca3f7275f899969c15aa41815515684
EgorNikitin94 <egornik1994@yandex.ru>
2022-07-15 16:48:26 +0300

    Initial Commit

---
+++ OmegaLogger/OmegaLogger.h
@@ -0,0 +1,18 @@
+//
+//  OmegaLogger.h
+//  OmegaLogger
+//
+//  Created by Егор Никитин on 7/15/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OmegaLogger.
+FOUNDATION_EXPORT double OmegaLoggerVersionNumber;
+
+//! Project version string for OmegaLogger.
+FOUNDATION_EXPORT const unsigned char OmegaLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OmegaLogger/PublicHeader.h>
+
+
